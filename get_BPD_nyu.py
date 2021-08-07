6c95848319ecdc69670a98e4622776534c68e01a
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-08-08 01:07:57 +0430

    Update get_BPD_nyu.py

--- get_BPD_nyu.py
+++ get_BPD_nyu.py
@@ -7,10 +7,11 @@ import torch.nn as nn
 import scipy.io as sio
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+import pickle
 
 
 DATASET = 'nyu'
-TEST_VIS_DIR = './test_nyu_pred_flux/'
+TEST_VIS_DIR = './train_sun_pred_flux/'
 
 def get_arguments():
     """Parse all the arguments provided from the CLI.
@@ -27,27 +28,27 @@ def get_arguments():
 args = get_arguments()
 
 def main():
-    
-    with open('/content/SuperBPD/nyu/val/val.txt', 'r') as f:
-        names = f.readlines()
 
-    with open('/content/SuperBPD/bpds.txt', 'w') as f:
+    before = dict()
+    after = dict()
+    
+    with open('/content/drive/MyDrive/datasets/sunrgbd/train/train.txt', 'r') as f:
+        names = f.readlines()
 
         for i_iter, image_name in enumerate(names):
 
             print(i_iter, image_name)
-            path = 'test_nyu_pred_flux/nyu/' + image_name[:-1] + '.mat'
+            path = 'train_sun_pred_flux/sun/' + image_name[4:-1] + '.mat'
 
             out = generate_cluster.main(path, True)
-            b = out['before']
-            a = out['after']
-            # print(np.unique(b).shape)#=(5335,)
-            # print(np.unique(a).shape)#=(403,)
-            f.write(image_name[:-1] + '\n')
-            f.write(str(a))
-            f.write('\n')
+            before[image_name[4:-1]] = out['before']
+            after[image_name[4:-1]] = out['after']
 
-
+    with open("/content/drive/MyDrive/SuperBPD/train_sun.pkl", "wb") as f:
+        pickle.dump(after, f)
         
+    with open("/content/drive/MyDrive/SuperBPD/train_sun_b.pkl", "wb") as f:
+        pickle.dump(before, f)
+
 if __name__ == '__main__':
     main()
