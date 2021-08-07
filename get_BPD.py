bc8d6a3a50d6ecf8945727d40a2f5fb7f25b0139
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-08-07 16:55:36 +0430

    Update get_BPD.py

--- get_BPD.py
+++ get_BPD.py
@@ -7,11 +7,11 @@ import torch.nn as nn
 import scipy.io as sio
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+import pickle
 
 
-DATASET = 'PascalContext'
-TEST_VIS_DIR = './test_pred_flux/'
-SNAPSHOT_DIR = './snapshots/'
+DATASET = 'sun'
+TEST_VIS_DIR = './train_sun_pred_flux/'
 
 def get_arguments():
     """Parse all the arguments provided from the CLI.
@@ -23,30 +23,33 @@ def get_arguments():
                         help="Dataset for training.")
     parser.add_argument("--test-vis-dir", type=str, default=TEST_VIS_DIR,
                         help="Directory for saving vis results during testing.")
-    parser.add_argument("--snapshot-dir", type=str, default=SNAPSHOT_DIR,
-                        help="Where to save snapshots of the model.")
     return parser.parse_args()
 
 args = get_arguments()
 
 def main():
     
-    with open('/content/SuperBPD/data/PascalContext/test.txt', 'r') as f:
+    after = dict()
+    before = dict()
+
+    with open('/content/drive/MyDrive/datasets/sunrgbd/train/train.txt', 'r') as f:
         names = f.readlines()
 
-    for i_iter, image_name in enumerate(names):
+        for i_iter, image_name in enumerate(names):
 
-        print(i_iter, image_name)
-        path = 'test_pred_flux/PascalContext/' + image_name[:-1] + '.mat'
+            print(i_iter, image_name)
+            path = '/content/SuperBPD/train_sun_pred_flux/sun/' + image_name[4:-1] + '.mat'
 
-        out = generate_cluster.main(path, True)
-        b = out['before']
-        a = out['after']
-        # print('***********************************\n', b)
-        # print('***********************************\n', a)
-        #print(np.unique(b).shape)=(1534,)
-        #print(np.unique(a).shape)=(90,)
-        hi
+            out = generate_cluster.main(path, True)
+            before[image_name[4:-1]] = out['before']
+            after[image_name[4:-1]] = out['after']
+
+    with open('/content/drive/MyDrive/datasets/sunrgbd/sun_train_bpds.pickle', 'wb') as f:
+        pickle.dump(after, f)
+
+    with open('/content/drive/MyDrive/datasets/sunrgbd/sun_train_bpds_b.pickle', 'wb') as f:
+        pickle.dump(before, f)
+
         
 if __name__ == '__main__':
     main()
